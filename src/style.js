abf773c4d1ad8570fd719c16d62e01e2a3193ef0
Ryochi <ryo1.t.zubu@gmail.com>
2021-08-08 16:12:45 +1000

    Flip Cards implemented

--- src/style.js
+++ src/style.js
@@ -13,5 +13,14 @@ function visibilityOn(){
 
 
 // Flip cards
+const cards = document.querySelectorAll('.card');
+function transition() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active')
+  } else {
+    this.classList.add('active');
+  }
+}
+cards.forEach(card => card.addEventListener('click', transition));
 
 
